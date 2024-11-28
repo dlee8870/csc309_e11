b7a58aed2dbe242c1cb69ab84444efde6318fdb8
dlee8870 <danielh.lee@mail.utoronto.ca>
2024-11-28 01:38:54 -0500

    EVERYTHING

---
+++ my-next-app/pages/api/csc309-e11.js
@@ -0,0 +1,8 @@
+export default function handler(req, res) {
+    res.status(200).send(`
+      <div id="auto-tester">
+        Hi. I am leedan67, and I have learned how to develop a full-stack web app
+        and deploy it!
+      </div>
+    `);
+  }
---
+++ my-next-app/pages/scripts.js
@@ -0,0 +1,12 @@
+function getBaseUrl() {
+    return 'https://your-vercel-deployment-url'; // Replace with your Vercel app URL
+  }
+  
+  function getUTorId() {
+    return 'leedan67';
+  }
+  
+  module.exports = {
+    getBaseUrl,
+    getUTorId,
+  };
