bae81fccddfc3c299f48162173c850f128cb0b38
dlee8870 <danielh.lee@mail.utoronto.ca>
2024-11-28 02:23:20 -0500

    Edits

--- my-next-app/pages/scripts.js
+++ my-next-app/pages/scripts.js
@@ -1,5 +1,5 @@
 function getBaseUrl() {
-    return 'https://csc309-e11-59kh-grm9hy3s3-dlee8870s-projects.vercel.app'; // Use your Vercel app URL
+    return 'https://csc309-e11-59kh.vercel.app/api/csc309-e11'; // Use your Vercel app URL
 }
 
 function getUTorId() {
