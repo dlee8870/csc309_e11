ec0c1dc0095a05cf6bc11dfaa87ab04d17822aa6
dlee8870 <danielh.lee@mail.utoronto.ca>
2024-11-28 01:55:25 -0500

    EVERYTHING

--- my-next-app/pages/scripts.js
+++ my-next-app/pages/scripts.js
@@ -1,12 +1,12 @@
 function getBaseUrl() {
-    return 'https://csc309-e11-59kh-gedahj7qu-dlee8870s-projects.vercel.app'; // Replace with your Vercel app URL
-  }
-  
-  function getUTorId() {
+    return 'https://csc309-e11-59kh.vercel.app'; // Use your Vercel app URL
+}
+
+function getUTorId() {
     return 'leedan67';
-  }
-  
-  module.exports = {
+}
+
+module.exports = {
     getBaseUrl,
     getUTorId,
-  };
+};
